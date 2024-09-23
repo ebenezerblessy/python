790af8816da6bd520b68c6b0e0c12580f1da24ad
Ebenezer A <141825924+ebenezerblessy@users.noreply.github.com>
2024-09-23 11:39:32 +0530

    Update pandas8.py

--- pandas8.py
+++ pandas8.py
@@ -1,15 +1,5 @@
 #read csv file in excel sheet
 import pandas as pd
-data = {
-    'ID': [1, 2, 3, 4],
-    'Name': ['ebi', 'blessy', 'angel', 'ebin'],
-    'Age': [21, 22, 23, 24],
-    'City': ['uthumalai', 'surandai', 'tenkasi', 'tirunelveli']
-}
 
-
-df = pd.DataFrame(data)
-csv_file_path ='Desktop/sample_data.csv'
-df.to_csv(csv_file_path, index=False)
-
-csv_file_path
+df = pd.read_csv('sample_data.csv')
+print(df.to_string(index=False))
